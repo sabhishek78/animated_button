ad009bd09dcb74c4806e89f935d14f982e815513
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-23 17:11:35 +0530

    in class animation

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Center(
-        child: AnimatedButton(),
+      title: 'Animation Demo',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children:<Widget>[AnimatedButton(Icons.favorite),
+      AnimatedButton(Icons.autorenew),
+      AnimatedButton(Icons.close)],
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class AnimatedButton extends StatefulWidget {
+  IconData icon;
+  AnimatedButton(this.icon);
   @override
   _AnimatedButtonState createState() => _AnimatedButtonState();
 }
 
-class _AnimatedButtonState extends State<AnimatedButton> {
+class _AnimatedButtonState extends State<AnimatedButton>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
 
+  @override
+  void initState() {
+    controller = AnimationController(
+        duration: Duration(milliseconds: 1200), vsync: this, value: 0);
+    controller.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+      else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+    controller.addListener(() {
+      print(controller.value);
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    CurvedAnimation smoothAnimation =
+    CurvedAnimation(parent: controller, curve: Curves.easeInExpo);
     return GestureDetector(
-      onTap: toggleVisible,
-      child: Icon(
-        Icons.location_on,
+      onTap: () {
+        controller.forward();
+      },
+      child: Container(
+        width: 150,
+        height: 150,
         color: Colors.white,
-        size: 60,
+        child: Transform.scale(
+          scale: Tween(begin: 1.0, end: 1.5).transform(controller.value),
+          child: Icon(
+            widget.icon,
+            color:Colors.red,
+            size: 60,
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   void toggleVisible() {
